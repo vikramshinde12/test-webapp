b7f58cb686244f24223a1a4176868653e054d0b8
vikram <vikram.shinde@morrisonsplc.co.uk>
2020-04-17 10:06:17 +0100

    added more line

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ app = Flask(__name__)
 
 DELAY_TIMER = os.environ.get('APP_START_DELAY') or 0
 COLOR = os.environ.get('COLOR') or "#1649a0"
+DAUGHTER_NAME = os.environ.get('DAUGHTER') or 'Sejal'
+AGE = os.environ.get('AGE') or 7
 
 
 @app.route('/')
 def main():
-    return render_template('hello.html', name=socket.gethostname(), color=COLOR)
-
+    return render_template('hello.html', name=socket.gethostname(), color=COLOR, daughter_name=DAUGHTER_NAME, age=AGE)
 
 if __name__ == '__main__':
     print("Application Ready !!")
